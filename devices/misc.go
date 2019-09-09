2e7f38ce96c5a8b0ddd3ae48492a5c17d7b1c007
dannypsnl <dannypsnl@gmail.com>
2019-09-09 14:00:08 +0800

    fix get PCI device ID
    For some NIC the readlink `/sys/class/net/<nic_name>` would get several
    PCI address and the correct one would be the second one, e.g.
    
        $ readlink -f /sys/class/net/ens224
        /sys/devices/pci0000:00/0000:00:17.0/0000:13:00.0/net/ens224
    
    To get the correct device ID, we have to check if the second one
    `0000:13:00.0` is a PCI address, return second one, else return the
    first one `0000:00:17.0`

--- devices/misc.go
+++ devices/misc.go
@@ -41,11 +41,16 @@ func GetDeviceID(nicName string) (string, error) {
 	}
 	// raw should be like /sys/devices/pci0002:00/0000:00:08.0/virtio2/net/ens8
 	raws := strings.Split(raw, "/")
-	if len(raws) < 5 {
+	if len(raws) < 6 {
 		return "", fmt.Errorf("path not correct")
 	}
-	return raws[4], nil
-
+	if IsPciID.Match([]byte(raws[5])) {
+		return raws[5], nil
+	} else if IsPciID.Match([]byte(raws[4])) {
+		return raws[4], nil
+	} else {
+		return "", fmt.Errorf("can't get device ID from path: %s", raw)
+	}
 }
 
 // IsModuleLoaded checks if the kernel has already loaded the driver or not.
